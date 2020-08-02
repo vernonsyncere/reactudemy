876e31f2831559d63d27a77c09ecb3a4c3f71820
Vernon Lewis <leesyncere@gmail.com>
2020-08-02 13:49:48 -0700

    different ways to render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component} from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-    </div>
+    // <div className="App">
+    //   <h1>Hi, I'm a React App</h1>
+    // </div>
+    React.createElement('div', {className: 'App'}, React.createElement('h1',null,"Do I work?"))
   );
 }
 
