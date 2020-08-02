daabe9c364c4d9ad2298e355aa96da9ef757177e
Vernon Lewis <leesyncere@gmail.com>
2020-08-02 13:51:52 -0700

    return to normal

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    // <div className="App">
-    //   <h1>Hi, I'm a React App</h1>
-    // </div>
-    React.createElement('div', {className: 'App'}, React.createElement('h1',null,"Do I work?"))
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+    </div>
+
   );
 }
 
