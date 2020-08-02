19f2d71271bec2723583c5507d4496cc731d1374
Vernon Lewis <leesyncere@gmail.com>
2020-08-02 14:01:57 -0700

    adding components and how they can be used

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
+      <p>This is working</p>
+      <Person/>
+      <Person/>
+      <Person/>
+      <Person/>
     </div>
 
   );
