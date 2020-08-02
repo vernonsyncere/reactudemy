14b0789057902013a8015604fc0738c2a64380a7
Vernon Lewis <leesyncere@gmail.com>
2020-08-02 14:11:49 -0700

    Passing in arguments as props and using things dynamically

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is working</p>
-      <Person/>
-      <Person/>
-      <Person/>
-      <Person/>
+      <Person name='Max' age='28'/>
+      <Person name='Manu' age='60'/>
+      <Person name='Stephanie' age='21'>My Hobbies are as such:</Person>
+      <Person name='Max' age='28'/>
     </div>
 
   );
